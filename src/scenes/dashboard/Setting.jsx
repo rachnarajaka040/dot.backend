3470a39a08814671d6d46fbc6336f608facb4c43
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-07-06 12:29:07 +0530

    image upload fixed

--- src/scenes/dashboard/Setting.jsx
+++ src/scenes/dashboard/Setting.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './setting.css';
+import axios from 'axios';
 
 function Setting() {
     const [selectedImages, setSelectedImages] = useState([]);
@@ -25,24 +26,50 @@ function Setting() {
         });
     };
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(e);
+
+        try {
+            const formData = new FormData();
+            formData.append('file', selectedImages[0]); // Use selectedImages instead of e.target.fileInput.files[0]
+
+            const response = await axios.post('http://localhost:4001/upload/slide', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <div className="setting-container">
             <div className="upload-container">
-                <label htmlFor="imageUpload" className="upload-button">
-                    <input
-                        type="file"
-                        id="imageUpload"
-                        accept="image/*"
-                        multiple
-                        onChange={handleImageUpload}
-                    />
-                    <i className="fas fa-cloud-upload-alt"></i> Upload Images
-                </label>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="imageUpload" className="upload-button">
+                        <input
+                            type="file"
+                            id="imageUpload"
+                            accept="image/*"
+                            name="file"
+                            multiple
+                            onChange={handleImageUpload}
+                        />
+                        <i className="fas fa-cloud-upload-alt"></i> Upload Images
+                    </label>
+                    <input type="submit" value="Upload" />
+                </form>
             </div>
 
             {selectedImages.length > 0 && (
                 <div>
-                    <h2 className="uploaded-heading" color="white">Uploaded Images:</h2>
+                    <h2 className="uploaded-heading" color="white">
+                        Uploaded Images:
+                    </h2>
                     <div className="image-container">
                         {selectedImages.map((image, index) => (
                             <div key={index} className="image-card">
